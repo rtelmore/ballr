81493ff2397fae3f2eb5ff47be4e82480afcc15f
Ryan Elmore <rtelmore@gmail.com>
2015-11-20 14:42:43 -0700

    no message

--- R/nba-player-per-game-stats.R
+++ R/nba-player-per-game-stats.R
@@ -1,12 +1,16 @@
+#' This function gets a player's career stats from basketball-reference.com
+#'
+#' @param player_link A link suffix, e.g. "/players/d/davisan02.html"
+#' @return An object of class tbl_df
+#' @examples
+#' GetNBAPlayerPerGameStats("/players/d/davisan02.html") # Anthony Davis
+#' GetNBAPlayerPerGameStats("/players/j/jamesle01.html") # Lebron James
+
 GetNBAPlayerPerGameStats <- function(player_link) {
-  ## Example player_link is of the form
-  ## Player: Anthony Davis
-  ## player_link <-
   player_url <- paste(default_base,
                       player_link,
                       sep = "")
   pg <- read_html(player_url)
-  player_stats <- tbl_df(html_table(pg, fill = T)[[2]]) %>%
-    filter(Age < max(Age, na.rm = T))
+  player_stats <- tbl_df(html_table(pg, fill = T)[[2]])
   return(player_stats)
 }
