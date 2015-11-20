3ab5d1f23be42c2fd13700a481f706a2744f04d3
Ryan Elmore <rtelmore@gmail.com>
2015-11-20 09:59:14 -0700

    general player stats

--- R/nba-player-per-game-stats.R
+++ R/nba-player-per-game-stats.R
@@ -1,10 +1,12 @@
-GetNBAPlayerPerGameStats <- function(player) {
+GetNBAPlayerPerGameStats <- function(player_link) {
+  ## Example player_link is of the form
+  ## Player: Anthony Davis
+  ## player_link <-
   player_url <- paste(default_base,
-                      player[, 2],
+                      player_link,
                       sep = "")
   pg <- read_html(player_url)
   player_stats <- tbl_df(html_table(pg, fill = T)[[2]]) %>%
-    filter(Age < max(Age, na.rm = T)) %>%
-    mutate(Player = player$Player)
+    filter(Age < max(Age, na.rm = T))
   return(player_stats)
 }
