8591a943380363ad4c65f15d2a880e1ba54af887
Ryan Elmore <rtelmore@gmail.com>
2015-11-20 15:48:55 -0700

    docs

--- R/nba-per-game-stats.R
+++ R/nba-per-game-stats.R
@@ -1,3 +1,5 @@
+#' NBA Player Statistics For a Given Season
+#'
 #' This function returns all player statistics from an NBA season on
 #' basketball-reference.com.  See an example table at:
 #' http://www.basketball-reference.com/leagues/NBA_2015_per_game.html
@@ -12,33 +14,33 @@
 #'   distinct()
 
 GetNBAPerGameStatistics <- function(season = 2016) {
-  nba_url <- paste(default_base,
+  nba_url <- paste(getOption("NBA_api_base"),
                    "/leagues/NBA_",
                    season,
                    "_per_game.html",
                    sep = "")
-  pg <- read_html(nba_url)
+  pg <- xml2::read_html(nba_url)
 
-  nba_stats <- tbl_df(html_table(pg, fill = T)[[1]])
+  nba_stats <- dplyr::tbl_df(rvest::html_table(pg, fill = T)[[1]])
   names(nba_stats)[c(11, 14, 17, 18, 21)] <- c("FGP",
                                                "3PP",
                                                "2PP",
                                                "eFGP",
                                                "FTP")
-  nba_stats <- filter(nba_stats,
-                      Player != "Player")
+  nba_stats <- dplyr::filter(nba_stats,
+                             Player != "Player")
   links <- pg %>%
-    html_nodes("tr.full_table") %>%
-    html_nodes("a") %>%
-    html_attr("href")
+    rvest::html_nodes("tr.full_table") %>%
+    rvest::html_nodes("a") %>%
+    rvest::html_attr("href")
   link_names <- pg %>%
-    html_nodes("tr.full_table") %>%
-    html_nodes("a") %>%
-    html_text()
+    rvest::html_nodes("tr.full_table") %>%
+    rvest::html_nodes("a") %>%
+    rvest::html_text()
   links_df <- tbl_df(data.frame(Player = as.character(link_names),
                                 link = as.character(links)))
   links_df[] <- lapply(links_df, as.character)
-  nba_stats <- left_join(nba_stats, links_df, by = "Player")
-  nba_stats <- mutate_each(nba_stats, funs(as.numeric), c(1, 4, 6:30))
+  nba_stats <- dplyr::left_join(nba_stats, links_df, by = "Player")
+  nba_stats <- dplyr::mutate_each(nba_stats, funs(as.numeric), c(1, 4, 6:30))
   return(nba_stats)
 }
--- R/nba-player-per-game-stats.R
+++ R/nba-player-per-game-stats.R
@@ -1,3 +1,5 @@
+#' NBA Player Career Statistics
+#'
 #' This function gets a player's career stats from basketball-reference.com
 #'
 #' @param player_link A link suffix, e.g. "/players/d/davisan02.html"
@@ -7,10 +9,10 @@
 #' GetNBAPlayerPerGameStats("/players/j/jamesle01.html") # Lebron James
 
 GetNBAPlayerPerGameStats <- function(player_link) {
-  player_url <- paste(default_base,
+  player_url <- paste(getOption("NBA_api_base"),
                       player_link,
                       sep = "")
-  pg <- read_html(player_url)
-  player_stats <- tbl_df(html_table(pg, fill = T)[[2]])
+  pg <- rvest::read_html(player_url)
+  player_stats <- dplyr::tbl_df(rvest::html_table(pg, fill = T)[[2]])
   return(player_stats)
 }
