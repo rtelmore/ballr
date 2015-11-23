5b026d15765f11e09258f0dd7771dad076d71481
Ryan Elmore <rtelmore@gmail.com>
2015-11-23 11:33:39 -0700

    more files

---
+++ R/nba-season-team-by-year.R
@@ -0,0 +1,25 @@
+#' NBA Team Statistics By Year
+#'
+#' This function returns a team's yearly statistics from basketball-reference.com
+#'
+#' @param team One of "ATL", "BOS", etc.
+#' @param season The particular season you are querying
+#' @return A team's game statistics for a particular season
+#' @examples
+#' NBASeasonTeamByYear("ATL", 2015)
+#' NBASeasonTeamByYear("NOP", 2015)
+
+NBASeasonTeamByYear <- function(team, season){
+  url <- paste(getOption("NBA_api_base"), "/teams/", team, "/", season,
+               "_games.html", sep="")
+  stats <- readHTMLTable(url)[['teams_games']][c(1, 2, 6:8, 10:14)]
+  stats <- stats[-c(21, 42, 63, 84), ]
+  stats[, c(1, 6:9)] <- apply(stats[, c(1, 6:9)], 2, as.numeric)
+  colnames(stats)[3] <- "Away_Indicator"
+  stats <- tbl_df(stats)
+  stats <- mutate(stats, Diff = Tm - Opp,
+                         AvgDiff = cumsum(Diff)/G,
+                         Away = cumsum(Away_Indicator == '@'),
+                         DaysBetweenGames = c(NA, as.vector(diff(mdy(Date)))))
+  return(stats)
+}
---
+++ R/nba-standings-by-date.R
@@ -0,0 +1,26 @@
+#' NBA Standings by Date
+#'
+#' This function the current NBA standings for the given date
+#'
+#' @param date_string A String of the form "2015-04-01"
+#' @return An list containing the standings in the Eastern and Western Conferences
+#' @examples
+#' NBAStandingsByDate("2010-01-31") # Jan 31, 2010
+#' NBAStandingsByDate("2015-11-19") # Nov 19, 2015
+
+NBAStandingsByDate <- function(date_string = Sys.Date()){
+  date_string <- ymd(date_string)
+  y <- year(date_string)
+  m <- month(date_string)
+  d <- day(date_string)
+  url <- paste(getOption("NBA_api_base"),
+               "/friv/standings.cgi?",
+               "month=", m,
+               "&day=", d,
+               "&year=", y,
+               "&lg_id=NBA", sep = "")
+  r <- read_html(url)
+  east <- html_table(r, fill = T)[[2]]
+  west <- html_table(r, fill = T)[[3]]
+  return(list(East = east, West = west))
+}
