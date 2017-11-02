6213b351fad54d38b521f93ed5f4c02e48b131d9
Peter DeWitt <dewittpe@gmail.com>
2017-11-02 01:49:07 -0600

    added a package doc file where imports from other packages will noted.

---
+++ R/ballr-pkg.R
@@ -0,0 +1,12 @@
+#' ballr
+#'
+#' @importFrom magrittr "%>%"
+#' @importFrom magrittr "%<>%"
+#' @importFrom rlang .data
+#' @docType package
+#' @name ballr
+NULL
+
+# Define globalVariables so R CMD check doesn't freak out
+utils::globalVariables(".")
+
