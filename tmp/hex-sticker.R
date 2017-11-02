e4b16cc9c25f5b49011c6bdaa5c342df4bea586a
Ryan Elmore <rtelmore@gmail.com>
2017-11-01 20:55:00 -0600

    sticker 2

--- tmp/hex-sticker.R
+++ tmp/hex-sticker.R
@@ -8,4 +8,11 @@ sticker("tmp/court.png", package="ballr",
         p_size = 6, p_x = 1, p_y = .4, p_color = "#FDB927",
         s_x = 1, s_y = 1, s_width = .7,
         h_fill = "#4D90CD", h_color = "#0F586C",
-        filename="fig/hex/court.png")
+        filename="man/figures/court.png")
+
+sticker("tmp/court.png", package="ballr",
+        p_size = 5, p_x = 1, p_y = 1, p_color = "#0F586C",
+        s_x = 1, s_y = 1, s_width = .8,
+        h_fill = "#4D90CD", h_color = "#FDB927",
+        filename="man/figures/court-2.png")
+
